4e1ae83242801a5946b0008e18badef1eb534188
Ilya Makarov <ilya.makarov.592@gmail.com>
2021-01-22 22:19:15 +0200

    a

--- include/game.h
+++ include/game.h
@@ -1,8 +1,14 @@
+#define black_define 0
 
-enum Side { BLACK, RED };
+constexpr auto black_constexpr = 0;
+
+enum Side { black_enum, RED };
 
 struct Game {
 
+    // public method
+    void start_game();
 private:
+    // private member
     Side side;
 };
