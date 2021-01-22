478c484e0058e596389f4448eeaab9d5a7027110
ksyyta <ksenijaivanova5@gmail.com>
2021-01-22 21:07:34 +0200

    first commit

---
+++ build/CMakeFiles/kakho4ew.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kakho4ew.dir/main.cpp.obj"
+  "kakho4ew.exe"
+  "kakho4ew.exe.manifest"
+  "kakho4ew.pdb"
+  "libkakho4ew.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kakho4ew.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ksyta/Desktop/Ksenija i Ilja
+# Build directory: C:/Users/ksyta/Desktop/Ksenija i Ilja/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
