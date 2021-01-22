52b18817ac720a082bbd0c32964f6569ad230882
Ilya Makarov <ilya.makarov.592@gmail.com>
2021-01-22 21:55:27 +0200

    added game.h

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ksyta/Desktop/Ksenija i Ilja
-# Build directory: C:/Users/ksyta/Desktop/Ksenija i Ilja/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ include/game.h
@@ -0,0 +1,8 @@
+
+enum Side { BLACK, RED };
+
+struct Game {
+
+private:
+    Side side;
+};
